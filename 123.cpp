7c7c9179e7c26a08d52fd51f3bf709abb450ff20
Abdusharif0095 <abdurafikiabdusharif2002@gmail.com>
2020-03-30 16:53:05 +0500

    Upload

---
+++ 123.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,i;
+    map<string,int>a;
+    cin>>n;
+    char x;
+    for (i = 0; i < n; i++) {
+        cin>>x;
+        cout<<x<<" ";
+    }
+return 0;
+}
